d57a02baf41040ca2c7d65b75921336b88aa291f
thuramos <ramosarthuroliveira@gmail.com>
2026-07-29 14:19:06 -0300

    Primeira iteração

---
+++ backend/gerenciador-musica-backend/src/main/java/gerenciador_musica_backend/GerenciadorMusicaBackendApplication.java
@@ -0,0 +1,13 @@
+package gerenciador_musica_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorMusicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorMusicaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/gerenciador-musica-backend/src/test/java/gerenciador_musica_backend/GerenciadorMusicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciador_musica_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorMusicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
