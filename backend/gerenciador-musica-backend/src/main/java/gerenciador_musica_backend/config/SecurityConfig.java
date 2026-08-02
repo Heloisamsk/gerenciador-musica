3103dc749d14c55fb053f9161844df732c80d6ae
thuramos <ramosarthuroliveira@gmail.com>
2026-08-02 18:20:05 -0300

    feat: implementa endpoint e validacoes de registro

--- backend/gerenciador-musica-backend/src/main/java/gerenciador_musica_backend/config/SecurityConfig.java
+++ backend/gerenciador-musica-backend/src/main/java/gerenciador_musica_backend/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package gerenciador_musica_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,15 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) 
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/register").permitAll() // Libera apenas o cadastro
+                .anyRequest().authenticated() 
+            );
+        return http.build();
+}
 }
