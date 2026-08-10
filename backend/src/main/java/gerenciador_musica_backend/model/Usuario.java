78e8b653c0a3f4492f6cbef751647a38627f2daf
alvwaro <drawvaro@gmail.com>
2026-08-09 21:50:52 -0300

    refactor(usuario): explicita mapeamento JPA

--- backend/src/main/java/gerenciador_musica_backend/model/Usuario.java
+++ backend/src/main/java/gerenciador_musica_backend/model/Usuario.java
@@ -1,55 +1,89 @@
 package gerenciador_musica_backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
 
-        @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(
+            name = "nome",
+            nullable = false,
+            length = 255
+    )
     private String nome;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            unique = true,
+            length = 255
+    )
     private String email;
+
+    @Column(
+            name = "senha",
+            nullable = false,
+            length = 255
+    )
     private String senha;
+
     @Enumerated(EnumType.STRING)
+    @Column(
+            name = "role",
+            nullable = false,
+            length = 255
+    )
     private Role role;
 
+    public Usuario() {
+    }
 
     public Long getId() {
         return id;
     }
+
     public String getNome() {
         return nome;
     }
-    public String getEmail() {
-        return email;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    public Role getRole() {
-        return role;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
+    public Role getRole() {
+        return role;
+    }
+
     public void setRole(Role role) {
         this.role = role;
     }
-
 }
