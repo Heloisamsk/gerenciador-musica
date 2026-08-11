88d3973cb7e9332dd6a2b0c6ec8fe2c4b72e3a7d
Heloisamsk <mariahelo956@gmail.com>
2026-08-11 11:09:00 -0300

    feat: cria repositorio para playlist

---
+++ backend/src/main/java/gerenciador_musica_backend/repository/PlaylistRepository.java
@@ -0,0 +1,14 @@
+package gerenciador_musica_backend.repository;
+
+import gerenciador_musica_backend.model.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+
+    List<Playlist> findByUsuarioId(Long usuarioId);
+
+    Optional<Playlist> findByIdAndUsuarioId(Long id, Long usuarioId);
+}
