c23bf3736c7b388154ec27f40cd010a8d7cc6c2c
alvwaro <drawvaro@gmail.com>
2026-08-21 13:44:10 -0300

    test(backend): activate isolated test profile

--- backend/src/test/java/gerenciador_musica_backend/GerenciadorMusicaBackendApplicationTests.java
+++ backend/src/test/java/gerenciador_musica_backend/GerenciadorMusicaBackendApplicationTests.java
@@ -2,8 +2,10 @@ package gerenciador_musica_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GerenciadorMusicaBackendApplicationTests {
 
 	@Test
