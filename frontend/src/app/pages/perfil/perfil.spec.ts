b6e18bb9f3653f562738361069db5b2a8d3aa5f7
vinic <vinniif.p@gmail.com>
2026-08-26 22:18:23 -0300

    Adicionado testes para o component perfil

--- frontend/src/app/pages/perfil/perfil.spec.ts
+++ frontend/src/app/pages/perfil/perfil.spec.ts
@@ -1,19 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { Perfil } from './perfil';
+import { AuthService } from '../../services/auth';
+import { vi } from 'vitest';
 
 describe('Perfil', () => {
   let component: Perfil;
   let fixture: ComponentFixture<Perfil>;
 
   beforeEach(async () => {
+    const authServiceMock = {};
+
     await TestBed.configureTestingModule({
       imports: [Perfil],
-    }).compileComponents();
+      providers: [
+        provideRouter([]),
+        { provide: AuthService, useValue: authServiceMock }
+      ]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(Perfil);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    vi.spyOn(Storage.prototype, 'getItem').mockImplementation((key: string) => {
+      if (key === 'nome') return 'Usuário Teste';
+      if (key === 'email') return 'teste@teste.com';
+      if (key === 'role') return 'USER';
+      return null;
+    });
+
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
   });
 
   it('should create', () => {
